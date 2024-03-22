ba9ba2a6aae7aefa3ccf15941d1834513595077b
Artur Correia <art.afo@outlook.pt>
2024-03-22 13:55:38 +0000

    Improve css for other screen sizes

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,11 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class ResetFormController extends Controller {
-	    connect() {
-		console.log("hello")
-    }
 	reset() {
 	    this.element.reset()
-   }
+       }
 
 }
